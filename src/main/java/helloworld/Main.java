1e66b01f7158e46c711a4127e185a0db226efa69
knastnt <456kot@mail.ru>
2020-01-07 19:53:05 +1000

    подключаемся к бд

--- src/main/java/helloworld/Main.java
+++ src/main/java/helloworld/Main.java
@@ -1,9 +1,20 @@
 package helloworld;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
         Message message = new Message();
         message.setText("Hello world!");
         System.out.println(message.getText());
+
+
+        Configuration configuration = new Configuration().configure();
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+
+        System.out.println();
     }
 }
