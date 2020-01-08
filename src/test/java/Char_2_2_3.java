29f8b6326ce2da29b19c20856fd6847b57d71ec6
knastnt <456kot@mail.ru>
2020-01-08 23:28:03 +1000

    запись чтение изменение

--- src/test/java/Char_2_2_3.java
+++ src/test/java/Char_2_2_3.java
@@ -1,11 +1,28 @@
 import helloworld.Message;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Char_2_2_3 {
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    @Before
+    public void init(){
+        emf = Persistence.createEntityManagerFactory("HelloWorldPU");
+        em = emf.createEntityManager();
+    }
+    @After
+    public void finish(){
+        em.close();
+        emf.close();
+    }
+
     @Test
     public void writeToDBMessages(){
         Message message = new Message();
@@ -13,8 +30,20 @@ public class Char_2_2_3 {
         message.setText("Hello world!");
         message2.setText("Hello world 2!");
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HelloWorldPU");
-        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.persist(message);
+        em.persist(message2);
+
+        em.getTransaction().commit();
+    }
+    @Test
+    public void writeToDBMessagesAndReadIt(){
+        Message message = new Message();
+        Message message2 = new Message();
+        message.setText("First Message");
+        message2.setText("Second Message");
 
         em.getTransaction().begin();
 
@@ -23,7 +52,18 @@ public class Char_2_2_3 {
 
         em.getTransaction().commit();
 
-        em.close();
-        emf.close();
+
+
+        em.getTransaction().begin();
+
+        List<Message> messages = em.createQuery("select m from Message m").getResultList();
+
+        Assert.assertEquals(messages.size(), 2);
+        Assert.assertEquals(messages.get(0).getText(), "First Message");
+
+        messages.get(0).setText("Text changed!");
+        message2.setText("Даже так! Текст меняется");
+
+        em.getTransaction().commit();
     }
 }
